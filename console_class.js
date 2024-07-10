cd5ea3ed43444f4e895f2bc0c935adf52cf64306
parth-mulashiya <parth1282m@gmail.com>
2024-07-10 12:17:39 +0530

    console_class

---
+++ console_class.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const out = fs.createWriteStream('./stdout.log');
+const err = fs.createWriteStream('./stderr.log');
+
+const myobject = new
+console.Console(out,err);
+myobject.log('This is the first example');
+myobject.log('This is the %s example','second');
+myobject.error(new Error('in this we creating some error'));
+const num = 'third';
+myobject.warn('this is the ${num}example');
--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// const fs = require('fs')
-// console.log('start reading a file...')
-
-// fs.readFile('test1.txt', 'utf-8',function(err,content){
-//     if(err){
-//         console.log('error happend during reading the file')
-//         return console.log(err)
-//     }
-//     console.log(content)
-// })
-// console.log('end of the file')
-
-
-
-
 const express = require('express');
 const app = express();
 
